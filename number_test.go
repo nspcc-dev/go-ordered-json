9bbd4ebfc9deb78726afa076d87e909f3095f122
Roman Khimov <roman@nspcc.ru>
2025-09-09 22:27:55 +0300

    *: use Go 1.24 b.Loop() in benchmarks
    Signed-off-by: Roman Khimov <roman@nspcc.ru>

--- number_test.go
+++ number_test.go
@@ -119,7 +119,7 @@ func TestNumberIsValid(t *testing.T) {
 
 func BenchmarkNumberIsValid(b *testing.B) {
 	s := "-61657.61667E+61673"
-	for range b.N {
+	for b.Loop() {
 		isValidNumber(s)
 	}
 }
@@ -127,7 +127,7 @@ func BenchmarkNumberIsValid(b *testing.B) {
 func BenchmarkNumberIsValidRegexp(b *testing.B) {
 	var jsonNumberRegexp = regexp.MustCompile(`^-?(?:0|[1-9]\d*)(?:\.\d+)?(?:[eE][+-]?\d+)?$`)
 	s := "-61657.61667E+61673"
-	for range b.N {
+	for b.Loop() {
 		jsonNumberRegexp.MatchString(s)
 	}
 }
